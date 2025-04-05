409f1b6b7125e71e36d512f9b34260a53765cb59
Ivan-Giovanni <zanguegiovanni@gmail.com>
2025-04-05 11:08:07 +0200

    Edit page being configured - Figuring out how to upload the image

--- services/imageService.js
+++ services/imageService.js
@@ -1,8 +1,27 @@
 export const getUserImageSrc = imagePath => {
     if (imagePath) {
-        return { uri: imagePath }
+        return { uri: imagePath };
     }
     else {
-        return require('../assets/images/defaultUser.png')
+        return require('../assets/images/defaultUser.png');
     }
 }
+
+export const uploadFile = async (folderName, fileUri, isImage = true) => {
+    try {
+        let fileName = getFilePath(folderName, isImage);
+
+
+    } catch (error) {
+        console.log('[imageService.js] file upload error', error);
+        return { success: false, message: '[imageService.js]could not upload media' };
+    }
+}
+
+
+
+export const getFilePath = (folderName, isImage) => {
+    return `/${folderName}/${(new Date()).getTime()}${isImage ? '.png' : '.mp4'}`;
+}
+
+
--- services/userService.js
+++ services/userService.js
@@ -19,3 +19,24 @@ export const getUserData = async (userId) => {
         return { success: false, message: error?.message }
     }
 }
+
+export const updateUser = async (userId, data) => {
+    try {
+        const { error } = await supabase
+            .from('users')
+            .update(data)
+            .eq('id', userId)
+            .single();
+        console.log('[userService.js] updated data: ', data);
+        if (error) {
+            return { success: false, message: error?.message }
+        }
+
+        return { success: true, data };
+
+    } catch (error) {
+        console.log('got error', error?.message);
+        return { success: false, message: error?.message }
+    }
+}
+
